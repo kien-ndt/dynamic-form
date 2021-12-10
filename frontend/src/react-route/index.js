90b9961a0a75bd953d4ea8313a4bc61761140c22
Huu Tuong Tu <83907151+huutuongtu@users.noreply.github.com>
2021-12-10 08:45:27 +0700

    abc
    testhoc

--- frontend/src/react-route/index.js
+++ frontend/src/react-route/index.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import CreateComponent from "../modules/create";
+import App from "../testsourcehoc/App"
 
 function ReactRoute(props){
     return(
         <BrowserRouter>
             <Routes>
                 <Route extract path="/create" element={<CreateComponent />} />
+                <Route extract path="/hoc" element={<App />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ frontend/src/testsourcehoc/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import List from "./List";
+
+
+function App() {
+  return <List />;
+}
+
+export default App;
